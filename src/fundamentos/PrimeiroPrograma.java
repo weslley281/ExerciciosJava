181c039f7172b8ad7e45699bcac306fa2daa611e
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-11-01 06:53:29 -0400

    a

--- src/fundamentos/PrimeiroPrograma.java
+++ src/fundamentos/PrimeiroPrograma.java
@@ -1,4 +1,12 @@
 package fundamentos;
 
 public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        //comentário de ma linha
+        /*
+        comentário de
+        multiplas linhas
+         */
+        System.out.println("Olá mundo");
+    }
 }
