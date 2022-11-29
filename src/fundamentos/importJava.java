d325c6e4a9d60b20ee480051d7c309d93e6ac7e7
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-11-29 06:45:10 -0400

    a

---
+++ src/fundamentos/importJava.java
@@ -0,0 +1,12 @@
+package fundamentos;
+import javax.swing.*;
+import java.util.Date;
+
+public class importJava {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println("A data é " + date);
+
+        JButton button = new JButton();
+    }
+}
