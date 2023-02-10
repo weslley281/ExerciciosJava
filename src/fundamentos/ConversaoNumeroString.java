1a93fe4cee453eb57755209c7554b93bf2c8a916
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2023-02-10 16:01:24 -0300

    a

---
+++ src/fundamentos/ConversaoNumeroString.java
@@ -0,0 +1,12 @@
+package fundamentos;
+
+public class ConversaoNumeroString {
+    public static void main(String[] args) {
+        Integer num1 = 10000;
+        System.out.println(num1.toString());
+        System.out.println("Quantidade: " + num1.toString().length());
+
+        int num2 = 10000;
+        System.out.println(Integer.toString(num2));
+    }
+}
