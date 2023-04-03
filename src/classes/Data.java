332df2161495cfcc6d99ef42120fc455d5a5bbbe
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-04-03 06:17:53 -0400

    a

--- src/classes/Data.java
+++ src/classes/Data.java
@@ -6,15 +6,16 @@ public class Data {
     int ano;
 
     Data(){
-        dia = 1;
-        mes = 1;
-        ano =1970;
+        // dia = 1;
+        //mes = 1;
+        //ano =1970;
+        this(1,1,1970);
     }
 
-    Data(int Dia, int Mes, int Ano){
-        dia = Dia;
-        mes = Mes;
-        ano = Ano;
+    Data(int dia, int mes, int ano){
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
     }
 
     String obterDataFormatada(){
