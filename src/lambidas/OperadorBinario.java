70a2a17c1a93c5b1ad42aae52dc06b109fdabe46
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2023-06-07 16:24:39 -0400

    a

---
+++ src/lambidas/OperadorBinario.java
@@ -0,0 +1,18 @@
+package lambidas;
+
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
+
+public class OperadorBinario {
+    public static void main(String[] args) {
+        BinaryOperator<Double> media = (n1, n2) -> (n1 + n2) / 2;
+        System.out.println(media.apply(9.8,5.7));
+
+        BiFunction<Double, Double, String > resultado = (n1, n2) -> {
+            double notaFinal = ((n1 + n2) / 2);
+            return notaFinal >= 7 ? "Aprovado" : "Reprovado";
+        };
+
+        System.out.println(resultado.apply(8.7,6.8));
+    }
+}
