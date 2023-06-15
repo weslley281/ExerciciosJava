963b58176d2732169060d3001a1a8161ea43c191
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-06-15 15:02:53 -0300

    a

--- src/excecao/ChecadaVsNaoChecada.java
+++ src/excecao/ChecadaVsNaoChecada.java
@@ -1,7 +1,27 @@
 package excecao;
 
 public class ChecadaVsNaoChecada {
-    public static void main(String[] args) {
-        System.out.println();
+    public static void main(String[] args) throws Exception {
+        try {
+            geraErro1();
+        }catch (RuntimeException e){
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            geraErro2();
+        }catch (RuntimeException e){
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("Fim...")
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     ;
+    }
+
+    static void geraErro1(){
+        throw new RuntimeException("Ocorreu um erro");
+    }
+    static void geraErro2() throws Exception{
+        throw new Exception("Ocorreu um erro");
     }
 }
